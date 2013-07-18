a5cb15c2220ece5d607b80505e7790941b0f970e
asu185 <frorgr@hotmail.com>
2013-07-19 01:57:22 +0800

    update dom_crawler, fix mysql decoding problem

--- dom_crawler.php
+++ dom_crawler.php
@@ -9,30 +9,35 @@ $html = file_get_html('http://www.youbike.com.tw/info04.php');
 $con = mysql_connect("localhost", "root", "1234");
 mysql_select_db("my_db", $con);
 
+mysql_query("SET NAMES 'UTF8'");
+mysql_query("ALTER TABLE UbikeInfo MODIFY SiteName VARCHAR(15) CHARACTER SET UTF8");
+
 mysql_query("TRUNCATE TABLE UbikeInfo");
 
 // Find all links 
 foreach($html->find('td.point01') as $element){
 	if($element->first_child()->plaintext){
-		$site_name = $element->first_child()->plaintext . ": ";
-		$ava_bikes = $element->next_sibling()->plaintext . " ";
-		$ava_parks = $element->next_sibling()->next_sibling()->plaintext . "\n";
+		$site_name = $element->first_child()->plaintext;
+		$ava_bikes = $element->next_sibling()->plaintext;
+		$ava_parks = $element->next_sibling()->next_sibling()->plaintext;
         $href = $element->first_child()->href;
         $map = file_get_html('http://www.youbike.com.tw/' . $href);
-        $lat = $map->find('input#lat')[0]->value;
-        $lng = $map->find('input#lng')[0]->value;
+        //$lat = $map->find('input#lat')[0]->value;
+        //$lng = $map->find('input#lng')[0]->value;
 
-        //foreach($map->find('input#lat') as $lat){
-        //    echo $lat->value;
-        //}
-        //foreach($map->find('input#lng') as $lng){
-        //    echo $lng->value;
-        //}
+        foreach($map->find('input#lat') as $lat){
+            $lat = $lat->value;
+        }
+        foreach($map->find('input#lng') as $lng){
+            $lng = $lng->value;
+        }
+        echo $site_name . ": " . $ava_bikes . " " . $ava_parks;
+        echo " " . $lat . " " . "$lng" . "\n";
 
         if(mysql_query("INSERT INTO UbikeInfo(SiteName,AvaBikes,AvaParks,Lat,Lng)
             VALUES('$site_name', '$ava_bikes', '$ava_parks', '$lat', '$lng')"))
         {
-            echo "Insert Success!\n";
+            //echo "Insert Success!\n";
         }
         else
             echo mysql_error();
